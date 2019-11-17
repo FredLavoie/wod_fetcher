162a2f9c7a79f3088ecd0aee3ec601bf39b7d09b
FredLavoie <flavoie3@gmail.com>
2019-11-17 10:17:46 -0500

    Replace htmlparser2 with cherio package

--- script.js
+++ script.js
@@ -1,37 +1,26 @@
 require('dotenv').config();
 const fs			= require('fs');
 const request 		= require('request');
-const htmlparser	= require('htmlparser2');
+const cherio		= require('cherio');
 
 let test = 'january-15-2018/';
-
-const parser = new htmlparser.Parser(
-	{
-		onprocessinginstruction(name, data) {
-			if (name === 'p') {
-				console.log('This worked!');
-				console.log(data);
-			}
-		},
-		ontext(text) {
-			console.log(text);
-		} 
-	},
-	{ decodeEntities: true }
-);
 
 const options = {
 	url: process.env.URL + test,
 };
 
-request(options, function(error, response, body) {
+request(options, function(error, response, html) {
     if (error) {
 		console.log('error: ', error);
 		console.log('statusCode: ', response && response.statusCode);
 		return;
 	}
+	if (response.statusCode !== 200) {
+		console.log();
+		
+	}
 
-	parser.write(body);
-	parser.end();
+
+
 });
 
