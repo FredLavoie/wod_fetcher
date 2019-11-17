ccf416fdff5b231415d63b13f9c354add7702f8a
FredLavoie <flavoie3@gmail.com>
2019-11-17 10:03:17 -0500

    Add htmlparser2 and start code to parse http res

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 require('dotenv').config();
-const fs		= require('fs');
-const request = require('request');
+const fs			= require('fs');
+const request 		= require('request');
+const htmlparser	= require('htmlparser2');
 
-let test = 'january-12-2019/';
+let test = 'january-15-2018/';
+
+const parser = new htmlparser.Parser(
+	{
+		onprocessinginstruction(name, data) {
+			if (name === 'p') {
+				console.log('This worked!');
+				console.log(data);
+			}
+		},
+		ontext(text) {
+			console.log(text);
+		} 
+	},
+	{ decodeEntities: true }
+);
 
 const options = {
-	url: process.env.URL,
-  }
+	url: process.env.URL + test,
+};
 
-  request(options, function(error, response, body) {
+request(options, function(error, response, body) {
     if (error) {
-      console.log('error: ', error);
-      console.log('statusCode: ', response && response.statusCode);
-    }
+		console.log('error: ', error);
+		console.log('statusCode: ', response && response.statusCode);
+		return;
+	}
 
-	console.log(response.statusCode);
+	parser.write(body);
+	parser.end();
+});
 
-    console.log('Download complete');
-    
-  });
