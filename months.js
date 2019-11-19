40f993f12ac88090d0c7154e92f9d904551c46d4
FredLavoie <flavoie3@gmail.com>
2019-11-18 22:00:18 -0500

    Progress commit for script
    - install moment.js for data formatting
    - add comment dividers in script file for readability
    - start iterator code to get each page by date
    - move fetch code to its own function

---
+++ months.js
@@ -0,0 +1,50 @@
+module.exports = [
+	{
+		name: 'january',
+		days: 31
+	},
+	{
+		name: 'february',
+		days: 29
+	},
+	{
+		name: 'march',
+		days: 31
+	},
+	{
+		name: 'april',
+		days: 30
+	},
+	{
+		name: 'may',
+		days: 31
+	},
+	{
+		name: 'june',
+		days: 30
+	},
+	{
+		name: 'july',
+		days: 31
+	},
+	{
+		name: 'august',
+		days: 31
+	},
+	{
+		name: 'september',
+		days: 30
+	},
+	{
+		name: 'october',
+		days: 31
+	},
+	{
+		name: 'novemer',
+		days: 30
+	},
+	{
+		name: 'december',
+		days: 31
+	}
+];
--- script.js
+++ script.js
@@ -1,26 +1,65 @@
+/* VARIABLE DECLARATION
+*************************************************************************/
 require('dotenv').config();
 const fs			= require('fs');
 const request 		= require('request');
 const cheerio		= require('cheerio');
+const months		= require('./months');
+const moment		= require('moment');
+const monthParam	= process.argv[2];
+const yearParam		= process.argv[3];
+
+const currentDay	= moment().format('Do');
+const currentDay2	= moment().format('DD');
+const currentMonth	= moment().format('MMM');
+const currentMonth2	= moment().format('MMMM');
+const currentYear	= moment().format('YYYY');
+
+/* 
+*************************************************************************/
+console.log('currentDay: ', currentDay);
+console.log('currentDay2: ', currentDay2);
+console.log('currentMonth: ', currentMonth);
+console.log('currentMonth2: ', currentMonth2);
+console.log('currentDay: ', currentYear);
+
+if(!yearParam || !monthParam) {
+	console.log('Input parameter(s) missing');
+} else if(!months.includes(monthParam)) {
+	console.log('Invalid month parameter');
+} else {
+	for (const month of months) {
+		for (let i = 1; i < months[month].days; i++) {
+			let dateVariable = `${month}-${}-${yearParam}`;
+
+		}
+	}
+}
 
 let test = 'january-15-2018/';
 
-const options = {
-	url: process.env.URL + test,
-};
+/* FUNCTIONS
+*************************************************************************/
+function fetchData(month, year) {
 
-request(options, function(error, response, html) {
-    if (error) {
-		console.log('error: ', error);
-		console.log('statusCode: ', response && response.statusCode);
-		return;
-	}
-	if (response.statusCode !== 200) {
-		console.log(response.statusCode);
-	}
-	const $ = cheerio.load(html);
-	const content = $('.entry-content');
-	console.log(content.text());
+	const options = {
+		url: process.env.URL + test,
+	};
 	
-});
+	request(options, function(error, response, html) {
+		if (error) {
+			console.log('error: ', error);
+			console.log('statusCode: ');
+			console.log(response && response.statusCode);
+			return;
+		}
+		if (response.statusCode !== 200) {
+			console.log(response.statusCode);
+			return;
+		}
+		const $ = cheerio.load(html);
+		const content = $('.entry-content');
+		console.log(content.text());		
+	});
 
+}
