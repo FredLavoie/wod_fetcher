64e0cf7f5faf1158c055881ba01d9f59be88821e
FredLavoie <flavoie3@gmail.com>
2019-11-24 06:16:59 -0500

    Refactor script to simplify

--- script.js
+++ script.js
@@ -7,11 +7,12 @@ const cheerio		= require('cheerio');
 const utils			= require('./utils.js');
 const moment		= require('moment');
 const currentDate	= moment().format('YYYY-MM-DD');
+const dateArray 	= utils.urlDateArray();
 
 /* FUNCTIONS
 **************************************************************************************************/
 
-function fetchData(dateURLEnd, currentDate) {
+function fetchData(dateURLEnd) {
 
 	const options = {
 		url: process.env.URL + dateURLEnd,
@@ -26,15 +27,17 @@ function fetchData(dateURLEnd, currentDate) {
 		}
 		const $ = cheerio.load(html);
 		const content = $('.entry-content');
-		const formattedContent = content.text().split('\t').join('').split('\n\n\n').join('')
-		.split('\n\n').join('');
-		const dataToBeWriten = '### ' + dateURLEnd + '\n' + formattedContent + '\n\n';
-		fs.appendFileSync(`./WODs/${currentDate}.md`, dataToBeWriten, 'utf-8', {'flags':'a+'});
-
+		const formattedContent = content.text()
+			.split('\t').join('')
+			.split('\n\n\n').join('')
+			.split('\n\n').join('');
+		const dataToBeWritten = '### ' + dateURLEnd + '\n' + formattedContent + '\n\n';
+		
+		fs.appendFileSync(`./WODs/${currentDate}.md`, dataToBeWritten, 'utf-8', {'flags':'a+'});
+		fs.appendFileSync(`./WODs/${currentDate}.txt`, dataToBeWritten, 'utf-8', {'flags':'a+'});
 	});
 }
 
-let dateArray = utils.urlDateArray();
-dateArray.forEach(dateURLEnd => {
-	fetchData(dateURLEnd, currentDate);
-});
+for (const date of dateArray) {
+	fetchData(date);
+}
