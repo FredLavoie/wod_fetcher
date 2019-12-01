3d9803c79cce2994b1380734658aa51e1f79d73e
FredLavoie <flavoie3@gmail.com>
2019-12-01 09:19:08 -0500

    Refactor script to use promise-based http request
    - in order to use async/await the http request function needs to return
    a promise

--- script.js
+++ script.js
@@ -2,42 +2,39 @@
 **************************************************************************************************/
 require('dotenv').config();
 const fs			= require('fs');
-const request 		= require('request');
+const rp		= require('request-promise-native');
 const cheerio		= require('cheerio');
-const utils			= require('./utils.js');
 const moment		= require('moment');
 const currentDate	= moment().format('YYYY-MM-DD');
+const utils			= require('./utils.js');
 const dateArray 	= utils.urlDateArray();
 
 /* FUNCTIONS
 **************************************************************************************************/
 
-function fetchData(dateURLEnd) {
+async function loppThroughArray() {
+	for (const date of dateArray) {
+		const options = {
+			url: process.env.URL + date
+		};
+		await asyncRequest(options, date);
+		console.log('fetching: ', date);
+	}
+}
 
-	const options = {
-		url: process.env.URL + dateURLEnd,
-	};
-	
-	request(options, function(error, response, html) {
-		if (error) {
-			return;
-		}
-		if (response.statusCode !== 200) {
-			return;
-		}
-		const $ = cheerio.load(html);
-		const content = $('.entry-content');
-		const formattedContent = content.text()
+async function asyncRequest(options, date) {
+	await rp(options)
+		.then(function(html){
+			const $ = cheerio.load(html);
+			const content = $('.entry-content');
+			const formattedContent = content.text()
 			.split('\t').join('')
 			.split('\n\n\n').join('')
 			.split('\n\n').join('');
-		const dataToBeWritten = '### ' + dateURLEnd + '\n' + formattedContent + '\n\n';
-		
-		fs.appendFileSync(`./WODs/${currentDate}.md`, dataToBeWritten, 'utf-8', {'flags':'a+'});
-		fs.appendFileSync(`./WODs/${currentDate}.txt`, dataToBeWritten, 'utf-8', {'flags':'a+'});
-	});
+			const dataToBeWritten = '### ' + date + '\n' + formattedContent + '\n\n';
+			fs.appendFileSync(`./WODs/${currentDate}.md`, dataToBeWritten, 'utf-8', {'flags':'a+'});
+			fs.appendFileSync(`./WODs/${currentDate}.txt`, dataToBeWritten, 'utf-8', {'flags':'a+'});
+		});
 }
 
-for (const date of dateArray) {
-	fetchData(date);
-}
+loppThroughArray();
--- utils.js
+++ utils.js
@@ -3,41 +3,41 @@ const moment		= require('moment');
 const currentYear	= Number(moment().format('YYYY'));
 
 exports.urlDateArray = function () {
-	let resultsArray = [];
-	let yearFrom = 2017;
+    let resultsArray = [];
+    let yearFrom = 2017;
 
-	for (let y = yearFrom; y <= currentYear; y++) {
-		for (const month in months) {
-			for (let d = 1; d <= months[month].days; d++) {
+    for (let y = yearFrom; y <= currentYear; y++) {
+        for (const month in months) {
+            for (let d = 1; d <= months[month].days; d++) {
 
-				let dateVariable1 = `${month}-${d}-${y}`;
-				resultsArray.push(dateVariable1);
-				if(d < 10) {
-					let dateVariable2 = `${month}-0${d}-${y}`;
-					resultsArray.push(dateVariable2);
-				}
-				let dateVariable3 = '';
-				switch (d) {
-					case 1:
-					case 21:
-					case 31:
-						dateVariable3 = `${month}-${d}st-${y}`;
-						break;
-					case 2:
-					case 22:
-							dateVariable3 = `${month}-${d}nd-${y}`;
-						break;
-					case 3:
-					case 23:
-							dateVariable3 = `${month}-${d}rd-${y}`;
-						break;		
-					default:
-							dateVariable3 = `${month}-${d}th-${y}`;
-						break;
-				}
-				resultsArray.push(dateVariable3);
-			}
-		}
-	}
-	return resultsArray;
+                let dateVariable1 = `${month}-${d}-${y}`;
+                resultsArray.push(dateVariable1);
+                if(d < 10) {
+                    let dateVariable2 = `${month}-0${d}-${y}`;
+                    resultsArray.push(dateVariable2);
+                }
+                let dateVariable3 = '';
+                switch (d) {
+                case 1:
+                case 21:
+                case 31:
+                    dateVariable3 = `${month}-${d}st-${y}`;
+                    break;
+                case 2:
+                case 22:
+                    dateVariable3 = `${month}-${d}nd-${y}`;
+                    break;
+                case 3:
+                case 23:
+                    dateVariable3 = `${month}-${d}rd-${y}`;
+                    break;
+                default:
+                    dateVariable3 = `${month}-${d}th-${y}`;
+                    break;
+                }
+                resultsArray.push(dateVariable3);
+            }
+        }
+    }
+    return resultsArray;
 };
