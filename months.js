f3c57c0bec1596a8126ce88bd2f594db0333c6c9
FredLavoie <flavoie3@gmail.com>
2020-02-28 21:46:19 -0500

    Add editor config file

--- months.js
+++ months.js
@@ -1,50 +1,50 @@
 module.exports = {
-	january: {
-		number: 1,
-		days: 31
-	},
-	february: {
-		number: 2,
-		days: 29
-	},
-	march: {
-		number: 3,
-		days: 31
-	},
-	april: {
-		number: 4,
-		days: 30
-	},
-	may: {
-		number: 5,
-		days: 31
-	},
-	june: {
-		number: 6,
-		days: 30
-	},
-	july: {
-		number: 7,
-		days: 31
-	},
-	august: {
-		number: 8,
-		days: 31
-	},
-	september: {
-		number: 9,
-		days: 30
-	},
-	october: {
-		number: 10,
-		days: 31
-	},
-	november: {
-		number: 11,
-		days: 30
-	},
-	december: {
-		number: 12,
-		days: 31
-	}
+  january: {
+    number: 1,
+    days: 31
+  },
+  february: {
+    number: 2,
+    days: 29
+  },
+  march: {
+    number: 3,
+    days: 31
+  },
+  april: {
+    number: 4,
+    days: 30
+  },
+  may: {
+    number: 5,
+    days: 31
+  },
+  june: {
+    number: 6,
+    days: 30
+  },
+  july: {
+    number: 7,
+    days: 31
+  },
+  august: {
+    number: 8,
+    days: 31
+  },
+  september: {
+    number: 9,
+    days: 30
+  },
+  october: {
+    number: 10,
+    days: 31
+  },
+  november: {
+    number: 11,
+    days: 30
+  },
+  december: {
+    number: 12,
+    days: 31
+  }
 };
--- script.js
+++ script.js
@@ -13,65 +13,65 @@ let dateArray				= [];
 /* FUNCTIONS
 **************************************************************************************************/
 async function loppThroughArray() {
-	let startTime = Date.now();
-	for (const date of dateArray) {
-		const options = {
-			url: process.env.URL + date
-		};
-		await asyncRequest(options, date);
-	}
-	
-	let endTimeMin = ((Date.now() - startTime) / (1000 * 60)).toFixed(2);
-	let endTimeSec = ((Date.now() - startTime) / (1000)).toFixed(2);
-	if(endTimeMin < 1) {
-		console.log(`Fetching completed in: ${endTimeSec} seconds`);
-	} else {
-		console.log(`Fetching completed in: ${endTimeMin} minutes`);
-	}
+  let startTime = Date.now();
+  for (const date of dateArray) {
+    const options = {
+      url: process.env.URL + date
+    };
+    await asyncRequest(options, date);
+  }
+
+  let endTimeMin = ((Date.now() - startTime) / (1000 * 60)).toFixed(2);
+  let endTimeSec = ((Date.now() - startTime) / (1000)).toFixed(2);
+  if(endTimeMin < 1) {
+    console.log(`Fetching completed in: ${endTimeSec} seconds`);
+  } else {
+    console.log(`Fetching completed in: ${endTimeMin} minutes`);
+  }
 
 }
 
 async function asyncRequest(options, date) {
-	await rp(options)
-		.then(function(html){
-			console.log('Writing: ', date);
-			const $ = cheerio.load(html);
-			const content = $('.entry-content');
-			const formattedContent = content.text()
-			.split('\t').join('')
-			.split('\n\n\n').join('')
-			.split('\n\n').join('');
-			const dataToBeWritten = '### ' + date + '\n' + formattedContent + '\n\n';
-			fs.appendFileSync(`./WODs/${currentDate}.md`, dataToBeWritten, 'utf-8', {'flags':'a+'});
-			fs.appendFileSync(`./WODs/${currentDate}.txt`, dataToBeWritten, 'utf-8', {'flags':'a+'});
-		})
-		.catch(function(){
-			return;
-		});
+  await rp(options)
+    .then(function(html){
+      console.log('Writing: ', date);
+      const $ = cheerio.load(html);
+      const content = $('.entry-content');
+      const formattedContent = content.text()
+      .split('\t').join('')
+      .split('\n\n\n').join('')
+      .split('\n\n').join('');
+      const dataToBeWritten = '### ' + date + '\n' + formattedContent + '\n\n';
+      fs.appendFileSync(`./WODs/${currentDate}.md`, dataToBeWritten, 'utf-8', {'flags':'a+'});
+      fs.appendFileSync(`./WODs/${currentDate}.txt`, dataToBeWritten, 'utf-8', {'flags':'a+'});
+    })
+    .catch(function(){
+      return;
+    });
 }
 
 switch (inputParam) {
-	case 'all':
-		console.log('Fetching all WODs since 2017');
-		dateArray = utils.allDateArray();
-		loppThroughArray();
-		break;
-	case 'year':
-		console.log('Fetching all WODs this year');
-		dateArray = utils.yearDateArray();
-		loppThroughArray();
-		break;
-	case 'month':
-		console.log('Fetching all WODs this month');
-		dateArray = utils.monthDateArray();
-		loppThroughArray();
-		break;
-	case 'day':
-		console.log('Fetching today\'s WOD');
-		dateArray = utils.dayDateArray();
-		loppThroughArray();
-		break;
-	default:
-		console.log('Wrong input parameters');
-		break;
+  case 'all':
+    console.log('Fetching all WODs since 2017');
+    dateArray = utils.allDateArray();
+    loppThroughArray();
+    break;
+  case 'year':
+    console.log('Fetching all WODs this year');
+    dateArray = utils.yearDateArray();
+    loppThroughArray();
+    break;
+  case 'month':
+    console.log('Fetching all WODs this month');
+    dateArray = utils.monthDateArray();
+    loppThroughArray();
+    break;
+  case 'day':
+    console.log('Fetching today\'s WOD');
+    dateArray = utils.dayDateArray();
+    loppThroughArray();
+    break;
+  default:
+    console.log('Wrong input parameters');
+    break;
 }
